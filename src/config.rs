8c3925778ca53508be742250b21c666f2b02d17f
DevCrafterTengfei <152072695+DevCrafterTengfei@users.noreply.github.com>
2025-11-13 17:36:55 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -109,8 +109,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["117.72.73.105"];
+pub const RS_PUB_KEY: &str = "0123456789";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
